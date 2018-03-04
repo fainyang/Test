42ea307712ccde5b4ec881832347535d955aab6c
FEI YANG <fainyang@foxmail.com>
2018-03-04 20:57:18 +0800

    add_score
    add_score

--- 11 exp bits.py
+++ 11 exp bits.py
@@ -9,7 +9,7 @@ def solution( exponent):
         return 1/11
     
     ans = solution(exponent >> 1)
-    print(exponent,ans)
+    #print(exponent,ans)
     ans = ans * ans
     if exponent & 1 == 1: #奇数补充一次
         ans = ans * 11
@@ -28,3 +28,28 @@ def mod(ans):
     	i+=1
     return count
 
+
+
+def solution1(N):
+    a=[1,1]
+    b=[]
+    #print(a)
+    for j in range(N-1):
+        t=0 #进位
+        a.append(0)
+        a.insert(0,0)#插入两个0是因为最低位肯定是1，最高位也许有进位
+        b=[]
+        for i in range(len(a)-1):
+            b.append((a[i]+a[i+1]+t)%10)
+            t=(a[i]+a[i+1]+t)//10
+        a=b
+        b=list(reversed(b))
+    
+    return(b)
+
+i=10000
+a=solution(i)
+#print(a)
+#b=solution1(i)
+#print(b)
+    
