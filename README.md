2c5a1e09a27714a0a37ec95a0ce705ce4ce933c9
FEI YANG <fainyang@foxmail.com>
2018-03-17 10:08:29 +0800

    add picture
    add picture

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Test
+# Python 自动回复机器人
+
+在python3环境下直接运行即可，如果没有module，需要先安装itchat包
--- tuling.py
+++ tuling.py
@@ -2,8 +2,9 @@
 import requests
 import itchat
 import random
+from itchat.content import *
 
-KEY = 'xxxxxxxxxxxxxxxx'#去图灵机器人网注册一个，得到apikey。
+KEY = '543bc357ffd4425da2a02db84320a72a'
 
 def get_response(msg):
     apiUrl = 'http://www.tuling123.com/openapi/api'
@@ -17,14 +18,41 @@ def get_response(msg):
         return r.get('text')
     except:
         return
+def group_id(name): #找到群ID
+    df = itchat.search_chatrooms(name=name)
+    return df[0]['UserName']
 
-@itchat.msg_register(itchat.content.TEXT)
+#自定义机器人后缀
+robots=['——[Pup]机器人[Pup]','——[Pup]急事语音通话[Pup]','——[Pup]不是本人[Pup]']
+
+@itchat.msg_register(TEXT)
 def tuling_reply(msg):
     defaultReply = 'I received: ' + msg['Text']
-    robots=['——By机器人','——有急事打call','——By反正不是本人']
     reply = get_response(msg['Text'])+random.choice(robots)
     return reply or defaultReply
-#enableCmdQR 在命令窗口显示二维码，如果改为itchat.auto_login()，则为系统弹出窗口显示二维码
-#但是Centos 没有图形化界面，只能命令窗口显示二维码
-itchat.auto_login(enableCmdQR=2) #enableCmdQR=True
+    # a or b的意思是，如果a有内容，那么返回a，否则返回b
+
+@itchat.msg_register([PICTURE, RECORDING, ATTACHMENT, VIDEO])
+def picture_reply(msg):
+    pic=str(random.randint(1,4))
+    itchat.send('@img@%s' % (pic+'.jpg'),msg['FromUserName'])
+    #从本地随机选择一张图片发送 '@img@图片地址'将会被识别为传送图片
+
+@itchat.msg_register([TEXT,PICTURE], isGroupChat=True)
+def group_text_reply(msg):
+    # 当然如果只想针对@你的人才回复，可以设置if msg['isAt']: 
+    #item = group_id('🦀️一只螃蟹，两只螃蟹🦀️')  # 根据自己的需求设置特定回复群
+    if msg['isAt'] :
+        reply = get_response(msg['Text'])+random.choice(robots)
+        itchat.send('%s' % reply,msg['FromUserName'] )
+    '''
+    #定点在某几个群里斗图
+    if msg['Type']=='Picture' and \
+    (msg['FromUserName']== group_id('🦀️一只螃蟹，两只螃蟹🦀️') or \
+    msg['FromUserName']== group_id('科大泸州程序猿交流群') or \
+    msg['FromUserName']== group_id('沉迷学习，日渐消瘦') ): 
+        pic=str(random.randint(1,4))
+        itchat.send('@img@%s' % (pic+'.jpg'),msg['FromUserName'])
+    '''
+itchat.auto_login() #enableCmdQR=True
 itchat.run()
